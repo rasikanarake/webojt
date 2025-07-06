d1800fd754576d245bfb17e59f968c2e5f7ec75b
rasikanarake <rasikanarake825@gmail.com>
2025-07-06 15:31:14 +0530

    Initial project files added

--- webojt/src/App.jsx
+++ webojt/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter,  Routes, Route, Router} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./components/pages/HomePage";
